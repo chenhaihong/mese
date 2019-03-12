1f315e7930e64fd559df27f70ab6bec5f92e07a2
erye <erye@tiiit.cn>
2019-03-12 21:22:28 +0800

    1.完成react ssr框架基础逻辑；2.TODO 配置、默认首页、favicn...

--- example/app/home.js
+++ example/app/home.js
@@ -3,9 +3,16 @@ import './common.less';
 import React from 'react';
 
 class Home extends React.Component {
+  handleClick = () => {
+    alert('点击事件生效');
+  }
+
   render() {
     return (
-      <h1>hello world</h1>
+      <div>
+        <h1>hello world</h1>
+        <p onClick={this.handleClick}>点击我，看点击事件是否生效</p>
+      </div>
     );
   }
 }
